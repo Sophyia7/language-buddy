22e74e79fb6b791b48827a551d456a88fbcd9aa5
Sophyia7 <iroegbusophia3@gmail.com>
2025-01-03 22:53:44 +0100

    Fixed redirect login to chat page

--- chats/urls.py
+++ chats/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from chats import views
 
+app_name = 'chats'
+
 urlpatterns = [
-   path('new/', views.conversation_view, name='new chat')
+   path('new/', views.conversation_view, name='chat')
 ]
