dffc334236655ece23211ee3f3dee2d095b976bf
Sophyia7 <iroegbusophia3@gmail.com>
2025-01-27 18:16:22 +0100

    Fixed django session bug

--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, logout 
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 import requests
